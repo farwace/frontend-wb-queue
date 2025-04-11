66fda017fb5e8627acf0ee7cb290cd75463f642d
panfilov.v <panfilov.v@bk.ru>
2025-04-11 07:44:48 +0300

    fix (wss) - connect only with use spectator component

--- src/echo.ts
+++ src/echo.ts
@@ -1,23 +1,23 @@
-import Echo from 'laravel-echo';
-import Pusher from 'pusher-js';
-
-declare global {
-    interface Window {
-        Pusher?: typeof Pusher;
-    }
-}
-
-window.Pusher = Pusher;
-
-const echo = new Echo({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: Number(import.meta.env.VITE_REVERB_PORT),
-    forceTLS: import.meta.env.VITE_REVERB_TLS === 'true',
-    disableStats: true,
-    enabledTransports: ['ws', 'wss'],
-
-});
-
-export default echo;
+// import Echo from 'laravel-echo';
+// import Pusher from 'pusher-js';
+//
+// declare global {
+//     interface Window {
+//         Pusher?: typeof Pusher;
+//     }
+// }
+//
+// window.Pusher = Pusher;
+//
+// const echo = new Echo({
+//     broadcaster: 'reverb',
+//     key: import.meta.env.VITE_REVERB_APP_KEY,
+//     wsHost: import.meta.env.VITE_REVERB_HOST,
+//     wsPort: Number(import.meta.env.VITE_REVERB_PORT),
+//     forceTLS: import.meta.env.VITE_REVERB_TLS === 'true',
+//     disableStats: true,
+//     enabledTransports: ['ws', 'wss'],
+//
+// });
+//
+// export default echo;
--- src/views/Spectator.vue
+++ src/views/Spectator.vue
@@ -14,9 +14,30 @@
 </template>
 
 <script lang="ts" setup>
-import {onMounted, ref} from 'vue'
+import {onMounted, onUnmounted, ref} from 'vue'
 import { useUserStore } from '../stores/user'
-import echo from "./../echo";
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
+
+declare global {
+  interface Window {
+    Pusher?: typeof Pusher;
+  }
+}
+
+window.Pusher = Pusher;
+
+const echo = new Echo({
+  broadcaster: 'reverb',
+  key: import.meta.env.VITE_REVERB_APP_KEY,
+  wsHost: import.meta.env.VITE_REVERB_HOST,
+  wsPort: Number(import.meta.env.VITE_REVERB_PORT),
+  forceTLS: import.meta.env.VITE_REVERB_TLS === 'true',
+  disableStats: true,
+  enabledTransports: ['ws', 'wss'],
+
+});
+
 
 const store = useUserStore()
 
@@ -53,6 +74,10 @@ onMounted(() => {
       isReady.value = true;
     }
   });
+});
+
+onUnmounted(() => {
+  echo.disconnect();
 })
 
 </script>
